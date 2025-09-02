1d139672f6d2f8c2259a6eb1037ae23d9b855de5
hoshino-st <mozzarella7575@gmail.com>
2025-09-02 14:12:40 +0900

    PlayerControlla 追加

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rbody; //PlayerについているRigidBody2Dを扱うための変数
+
+    float axisH ; //入力の方向を記憶するための変数
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     　　rbody = GetComponent<Rigidbody2D>(); //Playerについているコンポーネント情報を取得
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //もしも水平方向のキーが押されたら
+        //if (Input.GetAxisRaw ("Horizontal") != 0 )
+        
+            //Velocityの元となる値の取得 (右なら1.0f、左なら-1.0f、何もなければ0 )
+            axisH = Input.GetAxisRaw("Horizontal");
+
+            //Velocity 代入
+            rbody.linearVelocity = new Vector2( axisH, 0)
+        
+    }
+}
