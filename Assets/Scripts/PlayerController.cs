3af1a32799bd7c341e13ba64aeab7386c0e604ee
hoshino-st <mozzarella7575@gmail.com>
2025-09-02 15:24:00 +0900

    PlayerControlla の左右の動きを追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rbody; //PlayerについているRigidBody2Dを扱うための変数
 
     float axisH ; //入力の方向を記憶するための変数
+    public float speed = 3.0f ; //playerのスピードを調整
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,8 +22,12 @@ public class PlayerController : MonoBehaviour
             //Velocityの元となる値の取得 (右なら1.0f、左なら-1.0f、何もなければ0 )
             axisH = Input.GetAxisRaw("Horizontal");
 
-            //Velocity 代入
-            rbody.linearVelocity = new Vector2( axisH, 0)
-        
+    }
+
+    //1秒間に50回(50fps)繰り返すように制御しながら行う繰り返しメソッド
+    private void FixedUpdate()
+    {
+        //Velocity 代入
+        rbody.linearVelocity = new Vector2(axisH * speed , rbody.linearVelocity.y );
     }
 }
